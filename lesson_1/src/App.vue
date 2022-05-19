494341d1582720acea6d4dcc6cb2ce3d2b1109b4
shalashov <mik-shal2013@yandex.ru>
2022-05-19 15:57:27 +0300

    lesson 1

---
+++ lesson_1/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" :show="true"/>
+  <p>{{ msg }}</p>
+  <!-- <button @click="inc(2, $event)">{{ counter }}</button>
+  <button @click="reset">Reset counter</button> -->
+  <label for="someText">
+  <input 
+    id="someText"
+    type="text"
+    placeholder="text"
+    v-model="someText"
+    @focus="focus"
+    @blur="blur">
+    </label>
+    <p>{{ someText }}</p>
+    <CalcComponent />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import CalcComponent from './components/Calc.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: 'Hello Vue!',
+      counter: 0,
+      someText: ''
+    };
+  },
+  methods: {
+    inc(step, event) {
+      console.log(event);
+      this.counter += step;
+    },
+    reset () {
+      this.counter = 0;
+    },
+    focus(){
+      console.log ('focus');
+    },
+    blur () {
+      console.log('blur');
+    }
+  },
+  components: {
+    HelloWorld,
+    CalcComponent,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lesson_1/src/components/Calc.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="main">
+        <label for="op1">
+            <input id="op1" type="number" v-model.number="op1">
+        </label>
+        <label for="op2">
+            <input id="op2" type="number" v-model.number="op2">
+        </label>
+        = {{ result }}
+
+    </div>
+    <div>
+        <button @click="result = op1 + op2">+</button>
+        <button @click="sub">-</button>
+        <button @click="div">/</button>
+        <button @click="mult">*</button>
+        <button @click="power">Power</button>
+        <button @click="integerDiv">Integer division</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CalcComponent",
+    data() {
+        return {
+            op1: 0,
+            op2: 0,
+            result: 0,
+        };
+    },
+    methods: {
+        sub (){
+            this.result = this.op1 - this.op2
+        },
+        div (){
+            this.result = this.op1 / this.op2
+        },
+        mult (){
+            this.result = this.op1 * this.op2
+        },
+        power () {
+            this.result = this.op1 ** this.op2
+        },
+        integerDiv () {
+            this.result = parseInt(this.op1/this.op2)
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
