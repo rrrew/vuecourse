9c522f8aac6de6154c279dc39f55d5ae578e0056
shalashov <mik-shal2013@yandex.ru>
2022-05-29 13:46:49 +0300

    started lesson 3

---
+++ lesson_3/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <CounterComp/>
+  </div>
+</template>
+
+<script>
+import CounterComp from './components/Counter.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    
+  },
+  computed: {
+
+  },
+  components: {
+    CounterComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
